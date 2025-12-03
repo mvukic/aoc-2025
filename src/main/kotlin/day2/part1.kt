83f42dfb32a154327c12cef6689c23ea72a7c7a6
mvukic <matija.vukic2@gmail.com>
2025-12-03 23:05:33 +0100

    wip

--- src/main/kotlin/day2/part1.kt
+++ src/main/kotlin/day2/part1.kt
@@ -4,4 +4,27 @@ import com.mvukic.aoc.linesForDay
 
 fun main() {
     val lines = linesForDay(2)
+    val ranges = lines
+        .first()
+        .split(",")
+        .map { it.split("-") }
+        .map { it[0] to it[1] }
+        .map { it.first.toLong()..it.second.toLong() }
+
+    var s = 0L
+    for (range in ranges) {
+        println("${range.first} - ${range.last}")
+        val invalid = range.filter { v -> !isValid(v.toString()) }.sum()
+        s += invalid
+        println("Invalid sum in range $invalid")
+        println()
+    }
+
+    println("Total sum $s")
+}
+
+private fun isValid(value: String): Boolean {
+    if (value.length % 2 != 0) return true
+    if (value.take(value.length / 2) == value.drop(value.length / 2)) return false
+    return true
 }

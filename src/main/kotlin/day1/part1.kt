315037f9b078407e8abd8e44f970e45e0aeec04b
mvukic <matija.vukic2@gmail.com>
2025-12-03 22:17:30 +0100

    wip

--- src/main/kotlin/day1/part1.kt
+++ src/main/kotlin/day1/part1.kt
@@ -5,4 +5,30 @@ import com.mvukic.aoc.linesForDay
 fun main() {
     val lines = linesForDay(1)
 
+    val size = 100
+    var current = 50
+    var zero = 0
+
+
+    for (line in lines) {
+        val direction = line.first()
+        val amount = line.drop(1).toInt()
+
+        println("From: $current")
+        println("Direction: $direction, Amount: $amount")
+
+
+        current = when (direction) {
+            'L' -> (current - amount).mod(size)
+            'R' -> (current + amount).mod(size)
+            else -> error("Invalid Direction $direction")
+        }
+
+        println("To: $current")
+
+        if (current == 0) zero++
+        println()
+    }
+
+    println("Zeroes: $zero")
 }

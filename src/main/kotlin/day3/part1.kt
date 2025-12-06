e03e19a4b1cc316e89053a0b05eb7b061b502164
mvukic <matija.vukic2@gmail.com>
2025-12-06 16:10:24 +0100

    wip

--- src/main/kotlin/day3/part1.kt
+++ src/main/kotlin/day3/part1.kt
@@ -6,33 +6,26 @@ fun main() {
     val lines = linesForDay(3)
 
     var sum = 0L
-//    for (line in lines) {
-//        val digits = line.map { it.digitToInt() }
-//
-//        var currentLargest = 0.toDouble()
-//
-//        for (firstIndex in 0..line.length - 12) {
-//            var currentDigits = listOf(
-//                digits[firstIndex],
-//                digits.drop(firstIndex + 1).asSequence().max()
-//            )
-//            val firstDigit =
-//            val secondDigit =
-//            val n = firstDigit * 10 + secondDigit
-//            if (n > currentLargest) {
-//                currentLargest = n.toDouble()
-//            }
-//        }
-//        println(currentLargest)
-//        sum += currentLargest
-//    }
+    for (line in lines) {
+        val digits = line.map { it.digitToInt() }
+
+        var currentLargest = 0
+
+        for (firstIndex in 0..line.length - 2) {
+            val firstDigit = digits[firstIndex]
+            val secondDigit = digits.drop(firstIndex + 1).asSequence().max()
+            val n = firstDigit * 10 + secondDigit
+            if (n > currentLargest) {
+                currentLargest = n
+            }
+        }
+        println(currentLargest)
+        sum += currentLargest
+    }
 
     println(sum)
 }
 
-private fun digitsToNumber(digits: List<Int>): Double {
-    return digits.joinToString("").toDouble()
-}
 
 // First digit:
 // 2[34234234234278] -> index=0 digit=2
--- src/main/kotlin/day3/part2.kt
+++ src/main/kotlin/day3/part2.kt
@@ -1,20 +1,44 @@
 package com.mvukic.aoc.day3
 
 import com.mvukic.aoc.linesForDay
+import kotlin.math.pow
 
 fun main() {
     val lines = linesForDay(3)
 
-    var sum = 0L
+    var sum = 0.toULong()
     for (line in lines) {
         val digits = line.map { it.digitToInt() }
 
-        var currentLargest = 0
+        var currentLargest = 0.toULong()
 
-        for (firstIndex in 0..line.length - 2) {
-            val firstDigit = digits[firstIndex]
-            val secondDigit = digits.drop(firstIndex + 1).asSequence().max()
-            val n = firstDigit * 10 + secondDigit
+        for (index1 in 0..line.length - 12) {
+            val index2 = getIndexOfLargestIn(digits, index1)
+            val index3 = getIndexOfLargestIn(digits, index2)
+            val index4 = getIndexOfLargestIn(digits, index3)
+            val index5 = getIndexOfLargestIn(digits, index4)
+            val index6 = getIndexOfLargestIn(digits, index5)
+            val index7 = getIndexOfLargestIn(digits, index6)
+            val index8 = getIndexOfLargestIn(digits, index7)
+            val index9 = getIndexOfLargestIn(digits, index8)
+            val index10 = getIndexOfLargestIn(digits, index9)
+            val index11 = getIndexOfLargestIn(digits, index10)
+            val index12 = getIndexOfLargestIn(digits, index11)
+            val indexes = listOf(
+                index1,
+                index2,
+                index3,
+                index4,
+                index5,
+                index6,
+                index7,
+                index8,
+                index9,
+                index10,
+                index11,
+                index12
+            )
+            val n = digitsToNumber(digits, indexes)
             if (n > currentLargest) {
                 currentLargest = n
             }
@@ -24,4 +48,20 @@ fun main() {
     }
 
     println(sum)
+}
+
+private fun getIndexOfLargestIn(digits: List<Int>, prevIndex: Int): Int {
+    return digits
+        .drop(prevIndex + 1)
+        .indexOf(digits.asSequence().drop(prevIndex + 1).max())
+}
+
+private fun digitsToNumber(digits: List<Int>, indexes: List<Int>): ULong {
+    return indexes
+        .map { digits[it] }
+        .asReversed()
+        .asSequence()
+        .mapIndexed { index, value -> value * 10.toDouble().pow(index.toDouble()) }
+        .sum()
+        .toULong()
 }
